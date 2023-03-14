0ed3a5e0d6641356110b60dd8218688b4a1a7028
Arkadiusz <areq12x@gmail.com>
2023-03-14 21:47:09 +0100

    finished recruitment task

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Recruitment Task</title>
+        <title>Recruitment Task - Arkadiusz </title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
